d0b3e663d621e1ef5536ad42be83e9470a946b33
Joe <joseph_strauss@yahoo.com>
2017-04-18 14:04:46 -0400

    Add React-Rails boilerplate

---
+++ react/src/main.js
@@ -0,0 +1,10 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+$(function() {
+  ReactDOM.render(
+    <h1>Boo yaa</h1>,
+    document.getElementById('app')
+  );
+});
---
+++ react/webpack.config.js
@@ -0,0 +1,29 @@
+var config = {
+  entry: {
+    path: './react/src/main.js',
+  },
+  output: {
+    path: './app/assets/javascripts',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  },
+  devtool: 'eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  delete config.devtool;
+  var webpack = require('webpack');
+  config.plugins = [
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' })
+  ];
+}
+
+module.exports = config;
