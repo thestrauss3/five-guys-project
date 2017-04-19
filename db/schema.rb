73066f30abd15c42766a1c976909241f5d518f28
Joe <joseph_strauss@yahoo.com>
2017-04-19 10:34:55 -0400

    fix merge conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418195617) do
+ActiveRecord::Schema.define(version: 20170418195859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,4 +34,5 @@ ActiveRecord::Schema.define(version: 20170418195617) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
 end
