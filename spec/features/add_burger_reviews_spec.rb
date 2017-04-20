8827d3c167b3fe9e503f7e305b0e6b4043d648b5
Joe <joseph_strauss@yahoo.com>
2017-04-20 10:15:37 -0400

    Updated burger_reviews test

--- spec/features/add_burger_reviews_spec.rb
+++ spec/features/add_burger_reviews_spec.rb
@@ -1,18 +1,31 @@
 require "rails_helper"
 
-feature "User visits new review page" do
-  scenario "User sees a form to enter review information" do
-    wendys = Restaurant.create(name: "Wendys", location: "Boston", dining_type: "Might be a selction", description: "Really long lines and really long text", hours: "10am-10pm")
-    baconator = Burger.create name: "baconator", description: "lots of bacon", restaurant: wendys
-    visit new_burger_review_path(baconator)
-    expect(page).to have_content "Add a new review"
-    expect(page).to have_xpath "//input"
-    binding.pry
-    fill_in "Rating", with: "5"
 
-    fill_in 'Body', with: "This burger.. its so good. i eat a lot"
-    click_button 'Submit Review'
-    expect(page).to have_current_path(review_path(Review.find_by(body: "This burger.. its so good. i eat a lot")))
-    expect(page).to have_content "This burger.. its so good. i eat a lot"
+
+feature "User visits the new review page" do
+  let (:wendys) { Restaurant.create(name: "Wendys", location: "Boston", dining_type: "Might be a selction", description: "Really long lines and really long text", hours: "10am-10pm") }
+  let (:baconator) { Burger.create name: "baconator", description: "lots of bacon", restaurant: wendys }
+
+  feature "User visits the review page" do
+    scenario "User sees a form to enter review information" do
+      visit new_burger_review_path(baconator)
+      expect(page).to have_content "Add a new review"
+      expect(page).to have_xpath "//input"
+    end
+
+  end
+
+  feature "User submits a review" do
+    scenario "User successfully submits a review" do
+      visit new_burger_review_path(baconator)
+      fill_in "Rating", with: "5"
+      fill_in 'Body', with: "This burger.. its so good. i eat a lot"
+      click_button 'Submit Review'
+      expect(page).to have_current_path(review_path(Review.find_by(body: "This burger.. its so good. i eat a lot")))
+      expect(page).to have_content "This burger.. its so good. i eat a lot"
+    end
+    scenario "User unsuccessfully submits a review" do
+
+    end
   end
 end
