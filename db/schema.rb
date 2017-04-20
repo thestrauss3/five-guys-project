aec2e374d29a5f4a4963143dd40043797175cfaa
nlau77 <nlau777@gmail.com>
2017-04-20 10:44:52 -0400

    fixed up duplicates in header

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170419154035) do
+ActiveRecord::Schema.define(version: 20170420143441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
