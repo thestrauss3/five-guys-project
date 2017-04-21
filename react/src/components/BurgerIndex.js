60c95d4e7b76b6896cab205bba1376685ddab55f
Joe <joseph_strauss@yahoo.com>
2017-04-21 14:30:48 -0400

    working on api

--- react/src/components/BurgerIndex.js
+++ react/src/components/BurgerIndex.js
@@ -5,14 +5,31 @@ import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router';
 class BurgerIndex extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    }
+    this.state = { fortune: '' };
+  }
+
+  componentDidMount() {
+    fetch('/api/fortune.json')
+      .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          let errorMessage = `${response.status} (${response.statusText})`,
+              error = new Error(errorMessage);
+          throw(error);
+        }
+      })
+      .then(response => response.json())
+      .then(body => {
+        this.setState({ fortune: body.fortune });
+      })
+      .catch(error => console.error(`Error in fetch: ${error.message}`));
   }
 
   render() {
     return (
       <div>
-      <h1>da burgs</h1>
+       <h1>Your Fortune: {this.state.fortune}</h1>
       </div>
     )
   }
