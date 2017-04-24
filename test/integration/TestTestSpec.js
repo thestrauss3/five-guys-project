8c329d58bf967668ff30df347b3824d80aafbc7d
Joe <joseph_strauss@yahoo.com>
2017-04-24 14:47:40 -0400

    package.json enzyming

---
+++ test/integration/TestTestSpec.js
@@ -0,0 +1,12 @@
+import '../../react/src/main';
+import App from '../../react/src/App';
+import BurgerContainer from '../../react/src/containers/BurgerContainer';
+import BurgersContainer from '../../react/src/containers/BurgersContainer';
+
+describe('Burger', () => {
+  let variable
+  beforeEach(() => {
+    jasmineEnzyme();
+    wrapper = mount(<BurgerShow />)
+  })
+})
