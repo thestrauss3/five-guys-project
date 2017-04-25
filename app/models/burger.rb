262801c2fb48aeddfa4de5313473907ac06bf4ca
nlau77 <nlau777@gmail.com>
2017-04-25 13:53:59 -0400

    this might have broken somthing

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -5,6 +5,8 @@ class Burger < ApplicationRecord
 
   belongs_to :restaurant
   belongs_to :user, optional: true
+  mount_uploader :image_url, AvatarUploader
+
    #had to use optional condition to make tests pass in the
    # scenario where there were already burgers made without burger_rating
    # associated to a user
