647c750dec36278d6410e9ab973bec43e95967ce
nlau77 <nlau777@gmail.com>
2017-04-25 22:53:04 -0400

    testing search features

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,9 @@
 class SearchesController < ApplicationController
 
   def index
-    if params[:search]
+    if params[:search] == ""
+      @blank_message = "You have submitted a empty query"
+    else
       @burgers = Burger.search(params[:search])
       @restaurants = Restaurant.search(params[:search])
       if @burgers == [] && @restaurants == []
