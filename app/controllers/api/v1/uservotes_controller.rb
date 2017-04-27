0a54e9c9ae84f03e5ce7ac99a5b51efc46565f5a
nlau77 <nlau777@gmail.com>
2017-04-27 12:08:08 -0400

    reviews are not sorted by rating

--- app/controllers/api/v1/uservotes_controller.rb
+++ app/controllers/api/v1/uservotes_controller.rb
@@ -1,8 +1,6 @@
 class Api::V1::UservotesController < ApiController
 
   def index
-    # @vote_count = Burger.params[:id].reviews
-    # render json: @vote_count
   end
 
   def show
@@ -11,21 +9,9 @@ class Api::V1::UservotesController < ApiController
     @reviews.each do |review|
       total_votes= review.total_vote_count
       review.review_rating = total_votes
-      # binding.pry
       review.save
-      # review.update_attribute(review_rating: total_votes)
     end
+    @reviews = @reviews.order(review_rating: :desc)
     render json: @reviews
-
-    # @reviews = burger.reviews
-    # @review_total_votes = []
-    # @reviews.each do |review|
-    #   @review_total_votes << [review.id, review.total_vote_count]
-    # end
-    # # binding.pry
-    # render json: {:reviews => @reviews, :reviewTotalVotes => @review_total_votes}
   end
-
-
-
 end
